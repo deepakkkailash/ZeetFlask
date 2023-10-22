54ae3fa7eb11d8f9bf1762193fc1a8c8c4c61ae1
deepakkkailash <80799423+deepakkkailash@users.noreply.github.com>
2023-10-22 10:11:35 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,56 @@
-from flask import Flask,render_template
+from flask import Flask, request, render_template
+import os
+from werkzeug.utils import secure_filename
+from Test import make_binary, create_response
+from s3userupload import upload_user_S3
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
-def home():
+# Specify the upload folder
+UPLOAD_FOLDER = 'UserUploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Allowed file extensions
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+
+# Function to check if a file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    print(request.files)
+    # Check if a file was submitted
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    # If the user does not select a file, the browser submits an empty file without a filename
+    if file.filename == '':
+        return 'No selected file'
+
+    # Check if the file has an allowed extension
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        #The below code first uploads the uploaded image locally onto the system and then calls the function make_binary that makes sure that the image is converted into b
+        #binary format and then it returns the binary image onto temp. Now with create_response(temp) the response is created
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        upload_user_S3("UserUploads/"+filename)
+        temp = make_binary("UserUploads/"+filename)
+        val = create_response(temp)
+        return render_template("output.html",output=val)
+
+    else:
+        return 'Invalid file extension'
+
+
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+    app.run(port=8888,debug=True)
